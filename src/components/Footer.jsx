51a4180ff36fa8fa1151ae8e0102bb5170b163ec
apple <apple@apples-MacBook-Air.local>
2023-07-26 15:52:01 +0100

    create react app

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -59,7 +59,7 @@ function Footer() {
         <div class='social-media-wrap'>
           <div class='footer-logo'>
             <Link to='/' className='social-logo'>
-              TRVL
+              PG WORKS
               <i class='fab fa-typo3' />
             </Link>
           </div>
